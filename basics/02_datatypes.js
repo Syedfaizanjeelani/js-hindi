2af48ec592f4d328a344b42650e4c86db920aab2
Syed Faizan <139449692+Syedfaizanjeelani@users.noreply.github.com>
2024-02-05 14:23:15 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all code as newer version
+
+// alert("hello") //we are using node js not browser
+
+let name = "Faizan"
+let age = 21
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power 53
+// BigInt 
+// string => ""
+// boolean => true/false
+// null => standalon value(representation of empty value)
+// undefined => deaclare variable but not giving any value 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "undefined"); //undefined
+console.log(typeof "null"); //object
+
+
