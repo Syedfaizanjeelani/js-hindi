93c645f05f59b7ca46adf2f7f5283ba462a6eae1
Syed Faizan <139449692+Syedfaizanjeelani@users.noreply.github.com>
2024-02-20 06:12:56 +0000

    while and do while

---
+++ 05_loops/two.js
@@ -0,0 +1,22 @@
+
+// let index = 0
+// while (index <= 10) {
+//     // console.log(`Value of i is ${index}`)
+//     index++;
+// }
+
+// // let myArray = ["superman", "flash", "batman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     // console.log(`Value of ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// DO WHILE 
+let score = 1
+do {
+    console.log(` Value is ${score}`)
+    score ++
+
+} while (score <= 10);
