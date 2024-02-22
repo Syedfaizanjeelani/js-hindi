0821e5b23063ed1e1550f693c152036d536b08e6
Syed Faizan <139449692+Syedfaizanjeelani@users.noreply.github.com>
2024-02-22 06:36:16 +0000

    loop

---
+++ 05_loops/six.js
@@ -0,0 +1,47 @@
+// const coding  = ["js", "cpp", "python", "java", "c"]
+
+// const values = coding.forEach( (item) => {
+//         // console.log(item)                             //for each value return nhi krta
+//         return item
+// })
+// console.log(values)
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNum = myNums.filter( (num) => num > 4 )                 //FILTER VAUES KO RETURN KRDETA HAI
+//                                                                 //FILTER BHI CALLBACK FUNCTION LETA 
+//  console.log(newNum)
+
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//       newNums.push(num)
+//     }
+// })
+// console.log(newNums)                 //  FILTER YA FOR EACH MAI SE JO ASAAN LAGE WO METHOD KRE
+
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+// let userBook = books.filter( (bk) => bk.genre === "History" )
+// userBook = books.filter( (bk) =>bk.publish >= 2000)                           //ESE BHI KR SAKTE HO OR NECHE WALE TARIKE SE BHI KRSAKTE HO
+// console.log(userBook)
+
+
+let userBook = books.filter( (bk) => bk.genre === "History" )
+userBook = books.filter( (bk) => {
+   return bk.publish >= 1995 && bk.genre === "History"
+})
+console.log(userBook)
